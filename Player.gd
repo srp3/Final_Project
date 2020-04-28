fa87578051df1748e387eedc798b2aadf04cf77e
Emily Clinkscales <emclinks@github.com>
2020-04-28 16:06:23 -0400

    Player and Enemy additions

--- Player.gd
+++ Player.gd
@@ -17,7 +17,7 @@ var vel : Vector3 = Vector3()
 var mouseDelta : Vector2 = Vector2()
 
 onready var camera : Camera = get_node("Camera")
-onready var muzzle : Spatial = get_node("Camera/Muzzle")
+onready var muzzle : Spatial = get_node("flamethrower/Muzzle")
 onready var bulletScene = load("res://Bullet.tscn")
 onready var ui : Node = get_node("/root/Mainscene/CanvasLayer/UI")
 
